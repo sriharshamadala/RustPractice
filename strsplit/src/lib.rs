5c797e038478803d88bb64fc4b26c310a4b84d6f
sriharsha_madala <sriharsha.madala@gmail.com>
2020-04-22 23:43:12 -0700

    StrSplit lib: Basic lifetimes setup. Basic test passing. Tail test failing - needs fix.

--- strsplit/src/lib.rs
+++ strsplit/src/lib.rs
@@ -2,15 +2,18 @@
 /// Given a string and a delimiter, split the string into substrings
 ///
 
-struct StrSplit {
-    remainder: &str,
-    delimiter: &str,
+#[derive(Debug)]
+struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
 }
 
-impl StrSplit {
-    // We could return -> StrSplit but we have to edit in more places should the name change.
-    // If this is a long function, we may need to scroll to figure what type Self refers to.
-    pub fn new (haystack: &str, delimiter: &str) -> Self {
+// '_ is anonymous lifetime; We are asking the compiler to guess the lifetime
+// This only works if there is one possible value.
+impl<'a> StrSplit<'a> {
+    // We could return StrSplit but edit in more places should the name change.
+    // If this is a long function, we need to scroll to figure what type Self refers to.
+    pub fn new (haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
             remainder: haystack,
             delimiter
@@ -19,20 +22,25 @@ impl StrSplit {
 }
 
 // This allows us to do, "for part in StrSplit {}"
-impl Iterator for StrSplit {
-    type Item = &str;
-    fn next(&mut Self) -> Option<Self::Item> {
-        if let Some(next_delim) = Self.remainder.find(Self.delimiter) {
-            let until_delimiter = &Self.remainder[..next_delim];
-            Self.remainder = &Self.remainder[(next_delim + Self.delimiter.len())..];
+impl<'a> Iterator for StrSplit<'a> {
+    // We return a str, but what is the expected lifetime of this?
+    // It is obvious to us that this has remainder's lifetime but Rustc needs to know.
+    type Item = &'a str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
             Some(until_delimiter)
-        } else if Self.remainder.is_empty() {
+        } else if self.remainder.is_empty() {
             None
         } else {
             // Why bother with emptying the remainder in the last step?
             // Easy to return Some(Self.remainder)
-            let rest = Self.remainder;
-            Self.remainder = &[];
+            let rest = self.remainder;
+            // Empty string has static lifetime.
+            // But we said we will return something with lifetime 'a.
+            // Reason this is ok is, so long as lifetime is >= its fine.
+            self.remainder = "";
             Some(rest)
         }
     }
@@ -48,7 +56,14 @@ fn basic_test() {
         //...
     }
     */
-    let letters = StrSplit::new(haystack, " ");
-    assert_eq!(letters, vec!["a", "b", "c", "d", "e"].into_iter());
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(haystack, " ").collect();
+    assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
 }
 
