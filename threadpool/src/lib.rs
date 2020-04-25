812d8bb7ab1765f7033ea1895cb3c25e2f4e846e
sriharsha_madala <sriharsha.madala@gmail.com>
2020-04-25 16:17:36 -0700

    ThreadPool lib: checkpoint: Need for Atomics

--- threadpool/src/lib.rs
+++ threadpool/src/lib.rs
@@ -7,13 +7,13 @@ use std::sync::Arc;
 
 pub struct ThreadPool {
     _handles: Vec<std::thread::JoinHandle<()>>,
-    sender: Sender<Box<dyn Fn()+ Send>>,
+    sender: Sender<Box<dyn FnMut()+ Send>>,
 }
 
 impl ThreadPool {
     pub fn new(num_threads: u8) -> Self {
         // Using Dyn dispatch;
-        let (sender, receiver) = channel::<Box<dyn Fn()+ Send>>();
+        let (sender, receiver) = channel::<Box<dyn FnMut()+ Send>>();
         // Arc allows multiple owners. Mutex allows exclusive access to receiver.
         let receiver = Arc::new(Mutex::new(receiver));
         
@@ -24,11 +24,11 @@ impl ThreadPool {
             std::thread::spawn(move || 
             loop {
                 // We copy the clone to each thread.
-                let work = clone.lock().unwrap().recv();
-                match work {
-                    Ok(work) => work(),
+                let mut work = match clone.lock().unwrap().recv() {
+                    Ok(work) => work,
                     Err(_) => break,
-                }
+                };
+                work();
             })
          })
         .collect();
@@ -37,7 +37,7 @@ impl ThreadPool {
         }
     }
     
-    pub fn execute<T: Fn() + Send + 'static>(&self, work: T) {
+    pub fn execute<T: FnMut() + Send + 'static>(&self, work: T) {
         self.sender.send(Box::new(work)).unwrap();
     }
 }
@@ -53,4 +53,18 @@ fn it_works() {
     pool.execute(foo);
 }
 
+#[test]
+fn inc_num_test() {
+    let mut n = 42;
+    let nref = Arc::new(n);
+
+    let pool = ThreadPool::new(10);
+
+    // Fail to derefMut in arc. It doesn't allow exlusive access like &mut does.
+    let foo = || {
+        **nref = **nref + 1;
+    };
+    pool.execute(foo);
+    pool.execute(foo);
+}
 
