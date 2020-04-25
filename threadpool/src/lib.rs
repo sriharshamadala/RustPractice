17a85361b676ff390b91359a542380b0812f8a68
sriharsha_madala <sriharsha.madala@gmail.com>
2020-04-25 15:02:50 -0700

    ThreadPool lib: checkpoint: Need for Arc

--- threadpool/src/lib.rs
+++ threadpool/src/lib.rs
@@ -2,6 +2,7 @@
 /// Rust uses OS threads for zero-cost abstraction
 
 use std::sync::mpsc::channel;
+use std::sync::Mutex;
 
 pub struct ThreadPool {
     _handles: Vec<std::thread::JoinHandle<()>>,
@@ -9,22 +10,16 @@ pub struct ThreadPool {
 
 impl ThreadPool {
     pub fn new(num_threads: u8) -> Self {
-        // Channel expects the type we will send over. 
-        // We want to pass a function using this channel so that the threads can execute it.
-        // But Fn() is a trait and hence not of fixed size; hence cannot be used Channel<Fn()>
-        // We use the notion of dyn dispatch where Fn() is instead defined in heap; since we are only referring it is fixed size.
+        // Using Dyn dispatch;
         let (sender, receiver) = channel::<Box<dyn Fn()>>();
-        /* Convert this logic to map
-        for _ in 0..num_threads {
-            let handle = std::thread::spawn(|| {});
-        }
-        */
-        let _handles = (0..num_threads).map(|_| { std::thread::spawn(|| 
+        let receiver = Mutex::new(receiver);
+        
+        // Why is the closure move?
+        let _handles = (0..num_threads).map(|_| { std::thread::spawn(move || 
             loop {
                 // Blocking call. recv() returns Result<>. Ideally we want to handle Err<>;
-                // Correctly we get an error that receiver is not thread-safe. 
-                // It is written for single consumer, which isn't the case here.
-                let work = receiver.recv().unwrap();
+                // Mutex also shouldn't be unwrapped; If any mutex holder crashes, everthing crashes.
+                let work = receiver.lock().unwrap().recv().unwrap();
                 work();
             })
          })
@@ -34,9 +29,6 @@ impl ThreadPool {
         }
     }
     
-    // Instead of &self, if we use self here, thread_pool.execute()
-    // can only be called once since this function takes ownership
-    // of ThreadPool and won't return until complete.
     pub fn execute<T: Fn()>(&self, work: T) {
     }
 }
